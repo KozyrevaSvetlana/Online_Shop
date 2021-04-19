f49fbd7b9d55fc91fdabe5f76203e5ba11b59391
LanaKo0507 <lanako0507@gmail.com>
2021-04-19 14:10:37 +0300

    вывод строки в зависимости от текущего времени суток

---
+++ OnlineShop/OnlineShopWebApp/Controllers/startController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class startController : Controller
+    {
+        public string hello()
+        {
+            DateTime now = new DateTime();
+            now = DateTime.Now;
+            string result = "пустая строка";
+            int timeResult = now.Hour / 6;
+            switch (timeResult)
+            {
+                case 0:
+                    result = "Доброй ночи";
+                    break;
+                case 1:
+                    result = "Доброе утро";
+                    break;
+                case 2:
+                    result = "Добрый день";
+                    break;
+                case 3:
+                    result = "Добрый вечер";
+                    break;
+            }
+            return result;
+        }
+    }
+}
