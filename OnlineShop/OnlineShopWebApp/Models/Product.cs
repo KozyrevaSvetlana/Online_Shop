64d30c9afc8b496088a565f9ec38c663956d7774
LanaKo0507 <lanako0507@gmail.com>
2021-04-20 11:07:55 +0300

    создала класс Товар

---
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Product
+    {
+        private static int counter;
+        public int Id { get; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+        public Product(string name, decimal cost, string description)
+        {
+            counter++;
+            Id = counter;
+            Name = name;
+            Cost = cost;
+            Description = description;
+        }
+    }
+}
