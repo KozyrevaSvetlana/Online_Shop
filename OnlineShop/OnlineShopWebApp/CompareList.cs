74f9dd1734341ab93fadac5c0d6c39f2031ece45
LanaKo0507 <lanako0507@gmail.com>
2021-05-01 19:47:57 +0300

    создала список товаров для сравнения

---
+++ OnlineShop/OnlineShopWebApp/CompareList.cs
@@ -0,0 +1,32 @@
+﻿using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineShopWebApp
+{
+    public class CompareList
+    {
+        private static List<Product> compareList = new List<Product>();
+
+        public static void Add(Product product)
+        {
+            var allProducts = ProductsRepository.GetAll();
+            var result = allProducts.FirstOrDefault(x => x.Id == product.Id);
+            compareList.Add(result);
+        }
+        public static List<Product> GetCompareList()
+        {
+            return compareList;
+        }
+        public static void Delete(int idProduct)
+        {
+            var product = compareList.FirstOrDefault(x => x.Id == idProduct);
+            compareList.Remove(product);
+        }
+        public static void Clear(int idProduct)
+        {
+            compareList.Clear();
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Controllers/СompareController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class СompareController : Controller
+    {
+        private readonly ProductsRepository productRepository;
+        public СompareController()
+        {
+            productRepository = new ProductsRepository();
+        }
+
+        public IActionResult Index()
+        {
+            var cart = CompareList.GetCompareList();
+            return View(cart);
+        }
+
+        public IActionResult Add(int productId)
+        {
+            var product = productRepository.TryGetById(productId);
+            CompareList.Add(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
