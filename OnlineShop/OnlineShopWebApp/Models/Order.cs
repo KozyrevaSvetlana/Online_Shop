3a7e8c4531ec4260d25a913ddc2875b583d827d5
LanaKo0507 <lanako0507@gmail.com>
2021-05-06 22:25:24 +0300

    создала классы юзер и заказ

---
+++ OnlineShop/OnlineShopWebApp/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public DateTime OrderTime { get; set; }
+        public string Сomment { get; set; }
+
+        public Cart Cart { get; set; }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Models
+{
+    public class User
+    {
+        public static int Count = 0;
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public string Adress { get; set; }
+
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        private List<Order> Orders = new List<Order>();
+
+        public User(string name, string surname, string adress, string phone, string email)
+        {
+            Id = Count;
+            Name = name;
+            Surname = surname;
+            Adress = adress;
+            Email = email;
+            Count++;
+        }
+        public void AddOrder(Order order)
+        {
+            Orders.Add(order);
+        }
+    }
+}
