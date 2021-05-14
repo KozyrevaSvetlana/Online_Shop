c3bd8c8cd8f2133ec5cfe51bd5aad9991e707ae2
LanaKo0507 <lanako0507@gmail.com>
2021-05-14 16:27:56 +0300

    список продуктов в панели администратора отображается

---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/Products/ProductsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Views.Shared.ViewComponents.ProductsViewComponent
+{
+    public class ProductsViewComponent : ViewComponent
+    {
+        private readonly IProductsRepository productsRepository;
+
+        public ProductsViewComponent(IProductsRepository productsRepository)
+        {
+            this.productsRepository = productsRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var products = productsRepository.AllProducts;
+            return View("Products", products);
+        }
+    }
+}
