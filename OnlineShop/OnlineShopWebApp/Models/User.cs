d8eaa2245931f93d81bd2f61e2145e1372cc0c91
LanaKo0507 <lanako0507@gmail.com>
2021-05-20 12:55:10 +0300

    начала делать валидацию оформления заказа

--- OnlineShop/OnlineShopWebApp/Models/User.cs
+++ OnlineShop/OnlineShopWebApp/Models/User.cs
@@ -7,6 +7,8 @@ namespace OnlineShopWebApp.Models
         [Required(ErrorMessage = "Не указано имя")]
         [StringLength(44, MinimumLength = 2, ErrorMessage = "Имя должно быть не менее 2 символов и не более 44 символов")]
         public string Name { get; set; }
+
+
         [Required(ErrorMessage = "Не указан пароль")]
         [StringLength(35, MinimumLength = 4, ErrorMessage = "Пароль должен быть минимум 4 символа, максимум 35 символов")]
         public string Password { get; set; }
--- OnlineShop/OnlineShopWebApp/Models/UserContact.cs
+++ OnlineShop/OnlineShopWebApp/Models/UserContact.cs
@@ -1,11 +1,31 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
     public class UserContact
     {
+        [Required(ErrorMessage = "Не указано имя")]
+        [StringLength(44, MinimumLength = 2, ErrorMessage = "Имя должно быть не менее 2 и не более 44 символов")]
         public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Не указана фамилия")]
+        [StringLength(44, MinimumLength = 2, ErrorMessage = "Фамилия должна быть не менее 2 и не более 44 символов")]
         public string Surname { get; set; }
+
+
+        [Required(ErrorMessage = "Не указан адрес")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Неккоректный адрес. Он должен быть не менее 2 и не более 100 символов")]
         public string Adress { get; set; }
+
+
+        [Required(ErrorMessage = "Не указан телефон")]
+        [StringLength(16, MinimumLength = 3, ErrorMessage = "Телефон должен быть не менее 3 и не более 16 символов")]
         public string Phone { get; set; }
+
+
+        [Required(ErrorMessage = "Не указан email")]
+        [EmailAddress(ErrorMessage = "Укажите верный email")]
         public string Email { get; set; }
     }
 }
