c178f719a3d38dd39b0aa63d508fd0d1127b8b21
LanaKo0507 <lanako0507@gmail.com>
2021-05-22 16:39:17 +0300

    как запихнуть этот метод в конструктор чтобы не было ошибки?

--- OnlineShop/OnlineShopWebApp/InMemoryProductsRepository.cs
+++ OnlineShop/OnlineShopWebApp/InMemoryProductsRepository.cs
@@ -11,14 +11,16 @@ namespace OnlineShopWebApp.Models
         private List<Product> products = new List<Product>()
         {
                     new Product("Плюшевый мишка", 300, "Плюшевый мишка – символ нежности, трогательной заботы, тепла. " +
-                        "Многим он знаком с первых лет жизни.", "/img/Products/1.jpg",),
+                        "Многим он знаком с первых лет жизни.", "/img/Products/1.jpg", GetCategory("Игрушки"), GetSubcategory("мягкие игрушки", "Игрушки")),
                     new Product("Конструктор", 1000, "Любознательным малышам придется по душе конструктор.",
-                        "/img/Products/2.jpg"),
+                        "/img/Products/2.jpg", GetCategory("Игрушки"), GetSubcategory("Конструктор", "Игрушки")),
                     new Product("Пирамидка стаканчики", 200, "Пирамидка собирается из стаканчиков разного размера." +
                         "Только соблюдая четкую последовательность от большего стаканчика к меньшему у малыша получится башенка", 
-                        "/img/Products/3.jpg"),
-                    new Product("Водный пистолет", 150, "Длагодаря водному пистолету можно весело играть в друзьями летом на лужайке", "/img/Products/4.jpg"),
-                    new Product("Мяч детский", 170, "Мяч выполнен из прочного ПВХ и подходит для активных игр как дома, так и на воздухе", "/img/Products/5.jpg")
+                        "/img/Products/3.jpg", GetCategory("Игрушки"), GetSubcategory("Пирамидки", "Игрушки")),
+                    new Product("Водный пистолет", 150, "Длагодаря водному пистолету можно весело играть в друзьями летом на лужайке", "/img/Products/4.jpg",
+                        GetCategory("Игрушки"), GetSubcategory("Игрушечное оружие", "Игрушки")),
+                    new Product("Мяч детский", 170, "Мяч выполнен из прочного ПВХ и подходит для активных игр как дома, так и на воздухе",
+                        "/img/Products/5.jpg", GetCategory("Игрушки"), GetSubcategory("Мячи", "Игрушки"))
                 };
 
         public InMemoryProductsRepository(ICategoriesRepository categoriesRepository)
