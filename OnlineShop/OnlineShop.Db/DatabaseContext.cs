528f346ff8e63427c2d7f9529566cfc06eae27a9
LanaKo0507 <lanako0507@gmail.com>
2021-06-10 10:18:26 +0300

    Migration done

--- OnlineShop/OnlineShop.Db/DatabaseContext.cs
+++ OnlineShop/OnlineShop.Db/DatabaseContext.cs
@@ -15,7 +15,7 @@ namespace OnlineShop.Db
         public DatabaseContext(DbContextOptions<DatabaseContext> options)
             : base(options)
         {
-            Database.EnsureCreated();   // создаем базу данных при первом обращении
+            Database.Migrate();
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
