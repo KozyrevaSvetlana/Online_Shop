729668f1a6a2e1fcd7b407e06695c900565917a7
LanaKo0507 <lanako0507@gmail.com>
2021-06-27 18:05:00 +0300

    done

--- OnlineShop/OnlineShopWebApp/Areas/Admin/Controllers/ProductsController.cs
+++ OnlineShop/OnlineShopWebApp/Areas/Admin/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Db;
 using OnlineShop.Db.Models;
@@ -6,6 +7,8 @@ using OnlineShop.Db.Models.Interfaces;
 using OnlineShopWebApp.Helpers;
 using OnlineShopWebApp.Models;
 using System;
+using System.IO;
+using System.Linq;
 
 namespace OnlineShopWebApp.Areas.Admin.Controllers
 {
@@ -14,10 +17,12 @@ namespace OnlineShopWebApp.Areas.Admin.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductsRepository productsRepository;
+        private readonly IWebHostEnvironment appEnvironment;
 
-        public ProductsController(IProductsRepository products)
+        public ProductsController(IProductsRepository products, IWebHostEnvironment appEnvironment)
         {
             productsRepository = products;
+            this.appEnvironment = appEnvironment;
         }
         public IActionResult Index()
         {
@@ -34,6 +39,7 @@ namespace OnlineShopWebApp.Areas.Admin.Controllers
             return View(result.ToProductViewModel());
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult EditProduct(ProductViewModel editProduct)
         {
             var validResult = editProduct.IsValid();
@@ -48,26 +54,38 @@ namespace OnlineShopWebApp.Areas.Admin.Controllers
             {
                 var productDb = new Product
                 {
+                    Id= editProduct.Id,
                     Name = editProduct.Name,
                     Cost = editProduct.Cost,
                     Description = editProduct.Description,
                     Image = editProduct.Image
                 };
+                if (editProduct.UploadedFile != null)
+                {
+                    string productImagesPath = Path.Combine(appEnvironment.WebRootPath + "/img/Products/");
+                    var fileName = Guid.NewGuid() + "." + editProduct.UploadedFile.FileName.Split('.').Last();
+                    using (var fileStream = new FileStream(productImagesPath + fileName, FileMode.Create))
+                    {
+                        editProduct.UploadedFile.CopyTo(fileStream);
+                    }
+                    productDb.Image = "/img/Products/" + fileName;
+                }
                 productsRepository.Edit(productDb);
-                return RedirectToAction("Products", "Admin");
+                return RedirectToAction("Index");
             }
             return View("EditForm", editProduct);
         }
         public ActionResult DeleteProduct(Guid id)
         {
             //productsRepository.DeleteItem(id);
-            return RedirectToAction("Products", "Admin");
+            return RedirectToAction("Index");
         }
         public ActionResult AddProduct()
         {
             return View();
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult AddNewProduct(ProductViewModel newProduct)
         {
             var errorsResult = newProduct.IsValid();
@@ -81,17 +99,32 @@ namespace OnlineShopWebApp.Areas.Admin.Controllers
             }
             if (ModelState.IsValid)
             {
+                string fileName = "";
+                if (newProduct.UploadedFile != null)
+                {
+                    string productImagesPath = Path.Combine(appEnvironment.WebRootPath + "/img/Products/");
+                    if (!Directory.Exists(productImagesPath))
+                    {
+                        Directory.CreateDirectory(productImagesPath);
+                    }
+
+                    fileName = Guid.NewGuid() + "." + newProduct.UploadedFile.FileName.Split('.').Last();
+                    using (var fileStream = new FileStream(productImagesPath + fileName, FileMode.Create))
+                    {
+                        newProduct.UploadedFile.CopyTo(fileStream);
+                    }
+                }
                 var productDb = new Product
                 {
                     Name = newProduct.Name,
                     Cost = newProduct.Cost,
                     Description = newProduct.Description,
-                    Image = newProduct.Image
+                    Image = "/img/Products/" + fileName
                 };
                 productsRepository.Add(productDb);
-                return RedirectToAction("Products", "Admin");
+                return RedirectToAction("Index");
             }
-            return RedirectToAction("Products", "Admin");
+            return View("AddProduct", newProduct);
         }
     }
 }
