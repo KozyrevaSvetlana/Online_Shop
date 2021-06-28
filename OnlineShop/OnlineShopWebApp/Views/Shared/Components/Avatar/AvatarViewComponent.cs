11c36bc25e1db5f0f7ea6af7cde1caeea76f7860
LanaKo0507 <lanako0507@gmail.com>
2021-06-28 19:46:42 +0300

    add profile avatar to header

---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/Avatar/AvatarViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db.Models;
+using OnlineShop.Db.Models.Interfaces;
+using OnlineShopWebApp.Helpers;
+
+namespace OnlineShopWebApp.Views.Shared.ViewComponents.AvatarViewComponent
+{
+    public class AvatarViewComponent : ViewComponent
+    {
+        private readonly UserManager<User> userManager;
+
+        public AvatarViewComponent(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var user = userManager.GetUserAsync(HttpContext.User).Result;
+            if (user.Image == null || user.Image == "")
+            {
+                return View("Avatar", "/img/profile.webp");
+            }
+            return View("Avatar", user.Image);
+        }
+    }
+}
