8b5a2d270d5064c961c10bef3133a5fa379667df
LanaKo0507 <lanako0507@gmail.com>
2021-06-30 10:53:03 +0300

    =>Image

--- OnlineShop/OnlineShopWebApp/Views/Shared/Components/Image/ImageViewComponent.cs
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/Image/ImageViewComponent.cs
@@ -1,16 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Db.Models;
-using OnlineShop.Db.Models.Interfaces;
-using OnlineShopWebApp.Helpers;
 
-namespace OnlineShopWebApp.Views.Shared.ViewComponents.AvatarViewComponent
+namespace OnlineShopWebApp.Views.Shared.ViewComponents.ImageViewComponent
 {
-    public class AvatarViewComponent : ViewComponent
+    public class ImageViewComponent : ViewComponent
     {
         private readonly UserManager<User> userManager;
 
-        public AvatarViewComponent(UserManager<User> userManager)
+        public ImageViewComponent(UserManager<User> userManager)
         {
             this.userManager = userManager;
         }
@@ -20,9 +18,9 @@ namespace OnlineShopWebApp.Views.Shared.ViewComponents.AvatarViewComponent
             var user = userManager.GetUserAsync(HttpContext.User).Result;
             if (user.Image == null || user.Image == "")
             {
-                return View("Avatar", "/img/profile.webp");
+                return View("Image", "/img/profile.webp");
             }
-            return View("Avatar", user.Image);
+            return View("Image", user.Image);
         }
     }
 }
