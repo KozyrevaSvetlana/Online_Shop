d191551c51a704cadcb915ad2fa234b5c65bafc8
KozyrevaSvetlana <lanako0507@gmail.com>
2023-10-21 16:49:58 +0300

    отображение кол-ва в корзине

--- OnlineShop/OnlineShopWebApp/Views/Shared/Components/CartCount/CartCountViewComponent.cs
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/CartCount/CartCountViewComponent.cs
@@ -22,16 +22,7 @@ namespace OnlineShopWebApp.Views.Shared.ViewComponents.CartCountViewComponents
         {
             var productCounts = 0;
             var user = await userManager.GetUserAsync(HttpContext.User);
-            var cart = new Cart();
-            if (user != null)
-            {
-                cart = await cartsRepository.GetByIdAsync(null, user.Id);
-            }
-            else
-            {
-                var userId = Request.Cookies["id"];
-                cart = await cartsRepository.GetByIdAsync(null, userId);
-            }
+            var cart = await cartsRepository.GetByIdAsync(null, user.UserName ?? Request.Cookies["id"]);
             var cartViewModel = cart.ToCartViewModel();
             productCounts = cartViewModel?.Amount ?? 0;
             return View("CartCount", productCounts);
