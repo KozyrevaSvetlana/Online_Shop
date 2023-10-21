49730c5121e2c4da4f8489b47f5240729b01e008
KozyrevaSvetlana <lanako0507@gmail.com>
2023-10-21 16:33:51 +0300

    лишний юзинг

--- OnlineShop/OnlineShop.Db/IdentityContext.cs
+++ OnlineShop/OnlineShop.Db/IdentityContext.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using OnlineShop.Db.Helper;
 using OnlineShop.Db.Models;
